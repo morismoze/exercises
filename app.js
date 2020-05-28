0215ca6c07f97a4d983b0a40668fe5c0563305d6
Moris Mozé <moris.moze@fer.hr>
2020-05-29 00:38:04 +0200

    added javascript code so the user can change the theme using theme-changing-buttons

---
+++ app.js
@@ -0,0 +1,33 @@
+// DOM Elements
+
+const lightBtn = document.getElementById('light');
+const darkBtn = document.getElementById('dark');
+const solarBtn = document.getElementById('solar');
+
+// Cache the User's theme in local storage so if user refreshes the page, the theme stays
+
+const userTheme = localStorage.getItem('theme'); 
+
+if(userTheme !== undefined) {
+    document.body.className = userTheme;
+}
+// Event Listeners
+
+// .classLis.toggle only ADDS new class to existing class/classes
+lightBtn.addEventListener('click', () => {
+    document.body.className = "light";
+    // save user's theme in local storage (key-value pairs)
+    localStorage.setItem('theme', 'light');
+});
+
+darkBtn.addEventListener('click', () => {
+    document.body.className = "dark";
+    // save user's theme in local storage (key-value pairs)
+    localStorage.setItem('theme', 'dark');
+});
+
+solarBtn.addEventListener('click', () => {
+    document.body.className = "solar";
+    // save user's theme in local storage (key-value pairs)
+    localStorage.setItem('theme', 'solar');
+});
